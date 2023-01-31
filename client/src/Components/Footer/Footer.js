f9ef7fd10158b3977904103ac7238fda4389f86f
eslamicoo912 <eslamicoo3@gmail.com>
2023-01-31 21:03:37 +0200

    edit contact numbers

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -39,11 +39,11 @@ export default function Footer() {
             </li>
             <li>
               <AiOutlinePhone />
-              <p>+201204505484</p>
+              <p>01111143363+</p>
             </li>
             <li>
               <AiOutlineWhatsApp />
-              <p>+201092908943</p>
+              <p>01003797849+</p>
             </li>
             <li>
               <FaMapMarkerAlt />
